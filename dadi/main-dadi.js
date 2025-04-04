a2ef831aaf0c818c5939d0a925d358fa9f1be94b
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-04 17:22:45 +0200

    provo  a compattarlo

--- dadi/main-dadi.js
+++ dadi/main-dadi.js
@@ -3,9 +3,9 @@ console.log(playerNumber);
 const cpuNumber = Math.floor(Math.random() * 6) + 1;
 console.log(cpuNumber);
 
-let winMessage = alert("hai vinto!!");
-let lostMessage = alert("hai perso!!");
-let drawMessage = alert("pareggio!!");
+const winMessage = alert("hai vinto!!");
+const lostMessage = alert("hai perso!!");
+const drawMessage = alert("pareggio!!");
 for (let i = 0; playerNumber < 0 && cpuNumber < 0; i++);
 {
   if (playerNumber > cpuNumber) {
